20167511fca3ab41b4a9cdafb9b19ae68b3a0af3
phucthuan1st <phucthuan1st@gmail.com>
2022-07-19 17:21:03 +0700

    de day toi lam

--- API-Networking/ApiHandler.swift
+++ API-Networking/ApiHandler.swift
@@ -1,7 +1,39 @@
 
-
 import Foundation
+class APIHandler :URLSessionDataTask {
+	let url = "https://rtlab02.rtworkspace.com/api/query/datamodel?dm_name=test_ucdp_ged181&token=secret&limit=3&offset=1"
+	
+//	func startLoad(completion: (String)->()) {
+//		let url = URL(string: self.url)!
+//		let task = URLSession.shared.dataTask(with: url) { data, response, error in
+//			if let error = error {
+//				print(error)
+//				return
+//			}
+//			guard let httpResponse = response as? HTTPURLResponse,
+//				(200...299).contains(httpResponse.statusCode) else {
+//				print(String(describing: response))
+//				return
+//			}
+//			if let mimeType = httpResponse.mimeType, mimeType == "text/html",
+//			   let data = data,
+//			   let string = String(data: data, encoding: .utf8) {
+//				DispatchQueue.main.async {
+//					completion(string)
+//				}
+//			}
+//		}
+//		task.resume()
+//	}
+	
+	func startLoad() {
+		let url = URL(string: self.url)!
+		
+		let task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, _ in
+			
+		})
+		
+		task.resume()
+	}
 
-class ApiHandler {
-	var url:String = "https://rtlab02.rtworkspace.com/api/query/datamodel?dm_name=test_ucdp_ged181&token=secret&limit={limit}&offset={offset}"
 }
